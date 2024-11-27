e5945a438538138700ac93828795fbc1f55472ea
hyrondelle <n.hyr@free.fr>
2024-11-27 17:28:38 +0100

    categories

--- components/categories.tsx
+++ components/categories.tsx
@@ -1,9 +1,21 @@
+import { CATEGORIES } from '@/utils/categories';
+import Link from 'next/link';
 import React from 'react';
+import { Button } from './ui/button';
 
 const categories = () => {
     return (
-        <div>
-            categories
+        <div className='flex flex-row gap-2'>
+            {CATEGORIES.map((categories)=>(
+                        <div key={categories.id}>
+                        <Link 
+                        href={'/categories/'+categories.slug}>
+                            <Button variant='outline'>
+                            {categories.name}
+                            </Button>
+                        </Link>
+                        </div>
+                    ))}
         </div>
     );
 };
--- components/hero.tsx
+++ components/hero.tsx
@@ -8,10 +8,14 @@ const hero = () => {
     return (
         <div className='w-full relative'>
             <Image className='p-2 w-full h-[40vh] object-cover' src={bgphoto} alt='background picture'/>
-            <div className='w-1/3 h-1/2 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-slate-500/80'>
-                <p>Become A Better React Developper</p>
-                <Input type="email" placeholder="Email"/>
-                <Button>
+            <div className='w-1/3 h-2/5 absolute top-1/2 left-1/2 
+            -translate-x-1/2 -translate-y-1/2 bg-slate-500/80
+            rounded-sm p-1 gap-1 flex flex-col'>
+                <div className='font-extrabold text-center'>Become A Better React</div>
+                <div className='font-extrabold text-center'>Developper</div>
+                <Input type="email" placeholder="Email"
+                className='bg-slate-50'/>
+                <Button className='text-xs'>
                     Subscribe to our Newsletter
                 </Button>
             </div>

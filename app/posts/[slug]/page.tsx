55cdfb6458ac2bc2c48a10b075659fd7b3855b81
hyrondelle <n.hyr@free.fr>
2025-01-20 17:10:10 +0100

    posts

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import bgphoto from '@/public/backgrounddev.jpg'
 import Pagecontainer from '@/components/page-container';
+import { Avatar } from '@/components/ui/avatar';
+import { Eye, MessageCircle } from 'lucide-react';
 
 type props ={
     params:{
@@ -27,8 +29,8 @@ const postPage = ({params}:props) => {
     const {slug} = params
     return (
         <Pagecontainer>
-            <div className='w-full relative'>
-                <Image className='p-2 w-full h-[40vh] object-cover' src={bgphoto} alt='background picture'/>
+            <div className='w-full relative mt-4'>
+                <Image className='rounded-2xl p-2 w-full h-[40vh] object-cover' src={bgphoto} alt='background picture'/>
                 <div className='w-1/3 h-2/5 absolute top-1/2 left-1/2 
                 -translate-x-1/2 -translate-y-1/2 bg-slate-500/80
                 rounded-sm p-1 gap-1 flex flex-col justify-center'>
@@ -36,8 +38,31 @@ const postPage = ({params}:props) => {
                 </div>
             </div>
 
-            <div className=''>
+            <div className='flex justify-between border-b p-4'>
+                <div className='flex gap-2'>
+                    <Avatar className='bg-teal-600 items-center justify-center text-xl ml-4'>
+                    {post.author[0]}
+                    </Avatar>
+                    <div>
+                        <div>{post.author}</div>
+                        <div className='text-slate-400'>posté le {post.date}</div>
+                    </div>
+                </div>
+                <div className='mr-4 flex items-center gap-1'>
+                    <MessageCircle size={16}/>
+                    <p>{post.nbComments}</p>
+                    <Eye size={16}/>
+                    <p>{post.nbLikes}</p>
+                </div>
+            </div>
 
+            <div className='mt-4 pb-4 border-b'>
+                {post.content}
+            </div>
+
+            <div>
+                <p className='font-extrabold mt-4'>Comments</p>
+                <p className='mt-2'>Login to write a comment</p>
             </div>
         </Pagecontainer>
     );

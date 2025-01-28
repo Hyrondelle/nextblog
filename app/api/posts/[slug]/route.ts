c90254abedb82d0dda8098546ed49b94cfce2a99
Hyrondelle <n.hyr@free.fr>
2025-01-28 01:05:58 +0100

    get post route

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,6 +1,10 @@
 
+import { POSTS } from "@/utils/posts"
 import { NextResponse } from "next/server"
 
 export const GET = async (req:Request,{params}:{params:{slug:string}}) =>{
-    return NextResponse.json(params.slug,{status:200})
+    const post = await POSTS.find((poste)=>poste.id==parseInt(params.slug))
+    console.log(post);
+    
+    return NextResponse.json(post,{status:200})
 }

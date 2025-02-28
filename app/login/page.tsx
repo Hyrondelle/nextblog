7afe105c13a68d45dd5b7a3c06243bdb7d8c272d
hyrondelle <n.hyr@free.fr>
2025-02-28 11:55:49 +0100

    rename route

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,24 @@
+"use client"
 import Pagecontainer from '@/components/page-container';
 import { Button } from '@/components/ui/button';
 import { Github,Mails } from 'lucide-react';
+import { signIn } from 'next-auth/react';
 
-const login = () => {
+const Login = () => {
+    const SignIn = (provider:string) => {
+        signIn(provider)
+    }
     return (
-        
             <Pagecontainer>
                 <div className='flex flex-col py-10 mx-auto items-center'>
                     <div className='font-extrabold text-3xl max-w-md mb-4'>
                     Login
                     </div>
                     <div className='flex flex-col gap-4 max-w-md'>
-                    <Button>
+                    <Button onClick={()=>SignIn('github')}>
                         <Github size={16} />Signin With Github
                     </Button>
-                    <Button>
+                    <Button onClick={()=>SignIn('google')}>
                         <Mails size={16} />Signin With Google
                     </Button>
                     </div>
@@ -24,4 +28,4 @@ const login = () => {
     );
 };
 
-export default login;
+export default Login;

2a4bca0c43ee6c31b93291e0e93781ba59e01bc2
hyrondelle <n.hyr@free.fr>
2025-05-15 17:06:43 +0200

    add views

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -9,6 +9,15 @@ export const GET = async (req:Request,{params}:{params:{slug:string}}) =>{
         where:{slug} 
     })
     console.log(post);
-    
+    await prisma.post.update({
+        where: {
+            slug,
+          },
+          data: {
+            view: {
+                increment :1
+            },
+          },
+    })
     return NextResponse.json(post,{status:200})
 }
